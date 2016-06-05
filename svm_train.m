095f5a64cb9a2df01d6a2b88709bf397ac189d2c
slowbull <huozhouyuan@gmail.com>
2016-06-04 21:35:59 -0500

    modify svm_train, correct gradient of logistic

--- svm_train.m
+++ svm_train.m
@@ -25,13 +25,13 @@ while epoch < max_epochs
     for iter = 1:n
         index = index_permutation(iter);
         if strcmp(opt_method,'sgd')
-            gradient = w + C * (-Y(index)/(1+exp(Y(index)*w'*X(:,index))))*X(:,index);
+			gradient = compute_grad(w,X, Y, C index)
             w = w - lr * gradient;            
         elseif strcmp(opt_method,'sgd+mom')
             if beta < 0.9
                 beta = beta + lr;
             end
-            gradient = w + C * (-Y(index)/(1+exp(Y(index)*w'*X(:,index))))*X(:,index);
+			gradient = compute_grad(w,X, Y, C index)
             mom = beta * mom + lr * gradient;
             w = w - mom;
         elseif strcmp(opt_method,'svrg')
@@ -40,23 +40,23 @@ while epoch < max_epochs
                 hat_w = w;
                 gradient_full = zeros(d,1);
                for inner_iter = 1:n
-                    gradient = w + C * (-Y(inner_iter)/(1+exp(Y(inner_iter)*w'*X(:,inner_iter))))*X(:,inner_iter);                   
+            		gradient = w + C * (-Y(index)/(1+exp(Y(index)*w'*X(:,index))))*X(:,iner_iter);
                    gradient_full = gradient_full + gradient;
                end
                gradient_full = gradient_full / n;
             end
             
-            gradient = w + C * (-Y(index)/(1+exp(Y(index)*w'*X(:,index))))*X(:,index);
-            hat_gradient = hat_w + C * (-Y(index)/(1+exp(Y(index)*hat_w'*X(:,index))))*X(:,index);
+			gradient = compute_grad(w,X, Y, C index)
+			hat_gradient = compute_grad(hat_w, X, Y, C index)
             w = w - lr * (gradient - hat_gradient  + gradient_full);
         elseif strcmp(opt_method,'saga')
             if epoch == 1 && iter == 1
                 for i_num  = 1:n
-                    gradient = w + C * (-Y(index)/(1+exp(Y(index)*w'*X(:,index))))*X(:,index);
+					gradient = compute_grad(w,X, Y, C i_num)
                     alpha(:,i_num) = gradient;
                 end
             end
-            gradient = w + C * (-Y(index)/(1+exp(Y(index)*w'*X(:,index))))*X(:,index);
+			gradient = compute_grad(w,X, Y, C index)
             w = w - lr * (gradient - alpha(:,index) + sum(alpha,2)/n);
             alpha(:,index) = gradient;
        
@@ -67,7 +67,7 @@ while epoch < max_epochs
            
             neighbors = find(graph(:,index)==1);
 
-            gradient = w + C * (-Y(index)/(1+exp(Y(index)*w'*X(:,index))))*X(:,index);
+			gradient = compute_grad(w,X, Y, C index)
             w = w - lr * (gradient - alpha(:,index) + sum(alpha,2)/n);
 
            for i_neigh = 1:length(neighbors)
@@ -87,7 +87,7 @@ while epoch < max_epochs
                 beta2 = 0.999;
                 epsilon = 1e-8;
             end
-            gradient = w + C * (-Y(index)/(1+exp(Y(index)*w'*X(:,index))))*X(:,index);
+			gradient = compute_grad(w,X, Y, C index)
             mom_1 = beta1 * mom_1 + (1-beta1)*gradient;
             mom_2 = beta2 * mom_2 + (1-beta2)*gradient.^2;
             mom_1_bias = mom_1 / (1-beta1^iter);
@@ -109,3 +109,8 @@ obj = 0.5 * w'*w;
 obj = obj + C*sum(log(1+exp(-Y'.*(w'*X))));
 end
 
+
+function gradient = compute_grad(w,X, Y, C index)
+	tmp = -Y(index)*w'*X(:,index);
+	gradient = w + C * (-Y(index)*exp(tmp)/(1+exp(tmp)))*X(:,index);
+end
